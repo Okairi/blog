e4d28fba8c5744a6dd0285ce16dc9914e6f26307
Alessandro <anicho@ammper.com>
2025-03-11 14:27:24 -0500

    add styles for layout

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,11 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrl: './sidebar.component.css',
 })
 export class SidebarComponent {
+  public sidebarList: any[] = [
+    {
+      id: '1',
+      title: 'Blog 1123123123213213 titulo titulo  ',
+      author: 'Rias 1',
+    },
+    {
+      id: '2',
+      title: 'Blog 1123123123213213 titulo titulo  ',
+      author: 'Rias 2',
+    },
+    {
+      id: '3',
+      title: 'Blog 1123123123213213 titulo titulo  ',
+      author: 'Rias 3',
+    },
+  ];
 
+  constructor() {}
 }
