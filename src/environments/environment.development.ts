e2a7bcf38c1ee2d9470059375e75eae0ed7a2661
Alessandro <anicho@ammper.com>
2025-03-11 18:36:34 -0500

    ad data env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,12 @@
-export const environment = {};
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDmyED7sgjIcp2hTbNc0U_qP26MNQsO6sQ',
+    authDomain: 'blog-angular-e8d2b.firebaseapp.com',
+    projectId: 'blog-angular-e8d2b',
+    storageBucket: 'blog-angular-e8d2b.firebasestorage.app',
+    messagingSenderId: '1043602017913',
+    appId: '1:1043602017913:web:717928f85f07f9014ecea8',
+    measurementId: 'G-HMFL89KTFM',
+  },
+};
