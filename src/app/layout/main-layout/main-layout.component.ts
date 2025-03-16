5b8723671d7f2628c2fd5e21cb6534e16a06c222
Alessandro <anicho@ammper.com>
2025-03-15 20:58:15 -0500

    add toogle menu

--- src/app/layout/main-layout/main-layout.component.ts
+++ src/app/layout/main-layout/main-layout.component.ts
@@ -2,16 +2,10 @@ import { Component } from '@angular/core';
 import { AppHeaderComponent } from '../../shared/components/app-header/app-header.component';
 import { AppFooterComponent } from '../../shared/components/app-footer/app-footer.component';
 import { RouterOutlet } from '@angular/router';
-import { SidebarComponent } from '../../shared/components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [
-    AppHeaderComponent,
-    AppFooterComponent,
-    RouterOutlet,
-    SidebarComponent,
-  ],
+  imports: [AppHeaderComponent, AppFooterComponent, RouterOutlet],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css',
 })
--- src/app/shared/components/app-header/app-header.component.ts
+++ src/app/shared/components/app-header/app-header.component.ts
@@ -1,9 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './app-header.component.html',
   styleUrl: './app-header.component.css',
 })
-export class AppHeaderComponent {}
+export class AppHeaderComponent {
+  menuOpen = false;
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+}
