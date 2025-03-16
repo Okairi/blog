d716f7ab0c1380067a101f777d237dc92e7aecdb
Alessandro <anicho@ammper.com>
2025-03-16 01:34:08 -0500

    add system columns

--- src/app/features/blog/components/list-blog/list-blog.component.ts
+++ src/app/features/blog/components/list-blog/list-blog.component.ts
@@ -1,6 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import {
+  Firestore,
+  collection,
+  collectionData,
+  doc,
+  getDoc,
+} from '@angular/fire/firestore';
 import { Observable, Subscription } from 'rxjs';
 
 @Component({
@@ -13,51 +19,45 @@ export class ListBlogComponent implements OnInit, OnDestroy {
   blogs$: Observable<any[]>;
   private subscription!: Subscription;
   listBlogs: any[] = [];
+  usersMap: { [key: string]: string } = {}; // Mapa para almacenar user_id => nombre
 
   constructor(private firestore: Firestore) {
     const blogsCollection = collection(this.firestore, 'blogs');
     this.blogs$ = collectionData(blogsCollection);
   }
 
-  arrTest = [
-    {
-      title: 'cUAKKKKKKKKKK',
-      created_at: {
-        seconds: 1741724995,
-        nanoseconds: 648000000,
-      },
-      author_id: '1231231231231212',
-      valoracion: 4,
-      content:
-        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.,',
-    },
-    {
-      title: 'cUAKKKKKKKKKK',
-      created_at: {
-        seconds: 1741724995,
-        nanoseconds: 648000000,
-      },
-      author_id: '1231231231231212',
-      valoracion: 4,
-      content: 'Contenido raro',
-    },
-    {
-      title: 'cUAKKKKKKKKKK',
-      created_at: {
-        seconds: 1741724995,
-        nanoseconds: 648000000,
-      },
-      author_id: '1231231231231212',
-      valoracion: 4,
-      content: 'Contenido raro',
-    },
-  ];
-
   ngOnInit() {
-    this.subscription = this.blogs$.subscribe((blogs) => {
+    this.subscription = this.blogs$.subscribe(async (blogs) => {
       console.log('Blogs:', blogs);
-      this.listBlogs = blogs;
+
+      // Obtener nombres de autores
+      for (const blog of blogs) {
+        if (!this.usersMap[blog.author_id]) {
+          this.usersMap[blog.author_id] = await this.getUserName(
+            blog.author_id
+          );
+        }
+      }
+
+      // Asociar los nombres de los autores a los blogs
+      this.listBlogs = blogs.map((blog) => ({
+        ...blog,
+        authorName: this.usersMap[blog.author_id] || 'Desconocido',
+      }));
     });
+  }
+
+  async getUserName(userId: string): Promise<string> {
+    try {
+      const userDocRef = doc(this.firestore, 'users', userId);
+      const userDoc = await getDoc(userDocRef);
+      return userDoc.exists()
+        ? userDoc.data()['name'] || 'Sin nombre'
+        : 'Desconocido';
+    } catch (error) {
+      console.error('Error obteniendo usuario:', error);
+      return 'Error';
+    }
   }
 
   ngOnDestroy(): void {
